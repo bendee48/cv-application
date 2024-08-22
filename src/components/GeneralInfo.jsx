1a034fe2e92314f38c6959892f3bf6bab8a06f3b
bendee <benbanj@hotmail.com>
2024-08-22 16:34:34 +0100

    Adding component for general info
    - Adding a component for general info
    - Adding fields for name, email, phone
    - Adding state for a saved and unsaved form

---
+++ src/components/GeneralInfo.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+
+function GeneralInfo() {
+  const [personalInfo, setPersonalInfo] = useState({name: '', email: '', phone: ''});
+  const [saved, setSaved] = useState(false);
+
+  function handleNameChange(e) {
+    setPersonalInfo({
+      ...personalInfo,
+      name: e.target.value
+    })
+  }
+
+  function handleEmailChange(e) {
+    setPersonalInfo({
+      ...personalInfo,
+      email: e.target.value
+    })
+  }
+
+  function handlePhoneChange(e) {
+    setPersonalInfo({
+      ...personalInfo,
+      phone: e.target.value
+    })
+  }
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    if (!saved) {
+      setSaved(true);
+    } else {
+      setSaved(false);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Personal</h1>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          {!saved ? (<input id="name" type="text" value={personalInfo.name} onChange={handleNameChange}/>) : (<span>{personalInfo.name}</span>)}
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          {!saved ? (<input id="email" type="email" value={personalInfo.email} onChange={handleEmailChange}/>) : (<span>{personalInfo.email}</span>)}
+        </div>
+        <div>
+          <label htmlFor="phone">Phone:</label>
+          {!saved ? (<input id="phone" type="tel" value={personalInfo.phone} onChange={handlePhoneChange}/>) : (<span>{personalInfo.phone}</span>)}
+        </div>
+        <button>{!saved ? 'Save' : 'Edit'}</button>
+      </form>
+    </div>
+  )
+}
+
+export { GeneralInfo }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { GeneralInfo } from './components/GeneralInfo.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GeneralInfo />
   </StrictMode>,
 )
